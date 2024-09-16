d6dad83912c14c868042673d04f27e79c73a055f
zhen0z <zhen.zhang.cc@gmail.com>
2024-09-16 20:01:27 +0100

    Update jutulVWell.jl

--- src/FlowRules/Types/jutulVWell.jl
+++ src/FlowRules/Types/jutulVWell.jl
@@ -18,35 +18,24 @@
 
 
 # multiple wells
-export jutulVWell
-
 struct jutulVWell{D, T}
     irate::T
     name::Vector{Symbol}
-    loc::Vector
+    loc::Vector{NTuple{D, T}}
     startz
     endz
 end
 
-# Constructor for multiple wells with possible empty producer list
-jutulVWell(irate::T, injector_locs::Vector{NTuple{D, T}}, producer_locs::Vector{NTuple{D, T}}=Vector{NTuple{D, T}}(); startz=nothing, endz=nothing) where {D, T} =
-    let
-        # Define names: injectors followed by producers
-        injector_names = [:Injector for _ in 1:length(injector_locs)]
-        producer_names = [:Producer for _ in 1:length(producer_locs)]
-        names = vcat(injector_names, producer_names)
-        
-        # Combine injector and producer locations
-        loc = vcat(injector_locs, producer_locs)
-        
-        # Create the jutulVWell instance
-        jutulVWell{D+1, T}(irate, names, loc, startz, endz)
-    end
+# Constructor for a single well
+jutulVWell(irate::T, loc::NTuple{D, T}; startz=nothing, endz=nothing) where {D, T} = 
+    jutulVWell(irate, [:Injector], [loc], startz, endz)
 
-# Display function for jutulVWell
+# Constructor for multiple wells
+jutulVWell(irate::T, locs::Vector{NTuple{D, T}}; startz=nothing, endz=nothing) where {D, T} = 
+    jutulVWell{D+1, T}(irate, vcat(:Injector for _ in 1:length(locs)), locs, startz, endz)
+
 display(f::jutulVWell{D, T}) where {D, T} =
     println("$(D)D jutulVWell structure with $(length(f.loc)) wells and rate $(f.irate) m^3/s")
 
-# Equality check for jutulVWell
 ==(A::jutulVWell{D, T}, B::jutulVWell{D, T}) where {D,T} = 
     (A.irate == B.irate && A.name == B.name && A.loc == B.loc)
